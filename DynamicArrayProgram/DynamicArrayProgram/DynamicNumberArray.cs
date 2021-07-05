26c1162a970f1ace44a7561ae5b9bff433d9872e
Lakshmi L <51417725+lakshmil2514@users.noreply.github.com>
2021-07-05 15:03:43 +0530

    Add files via upload

---
+++ DynamicArrayProgram/DynamicArrayProgram/DynamicNumberArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArrayProgram
+{
+    public class DynamicNumberArray
+    {
+
+        int[] buffer;
+        public DynamicNumberArray()
+        {
+            buffer = new int[5];
+        }
+        
+        public DynamicNumberArray(int initialSize)
+        {
+            buffer = new int[initialSize];
+        }
+
+        public int this[int index]
+        {
+            /*if(index >= buffer.Length)
+            {
+              Array.Resize(ref buffer,index+5)
+            }*/
+            set
+            {
+                Array.Resize(ref buffer, index+5);
+                buffer[index] = value;
+            }
+            get
+            {
+                return buffer[index];
+            }
+
+        }
+    }
+}
+
+
+    
+
---
+++ DynamicArrayProgram/DynamicArrayProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DynamicArrayProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicNumberArray _dynamic = new DynamicNumberArray();
+            _dynamic[0] = 10;
+            _dynamic[1] = 20;
+            _dynamic[2] = 30;
+            _dynamic[3] = 40;
+            _dynamic[4] = 50;
+            _dynamic[5] = 60;
+
+            int value = _dynamic[5];
+            Console.WriteLine(value);
+        }
+    }
+}
